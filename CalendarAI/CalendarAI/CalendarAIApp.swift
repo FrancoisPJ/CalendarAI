b7d5c6fa3e0ec54b4484af2bc6f3362f859bfe62
François <55384534+FrancoisPJ@users.noreply.github.com>
2025-08-16 21:37:30 +0200

    Swift Base template

---
+++ CalendarAI/CalendarAI/CalendarAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAIApp.swift
+//  CalendarAI
+//
+//  Created by François Proust--Juvin on 16/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarAI/CalendarAITests/CalendarAITests.swift
@@ -0,0 +1,16 @@
+//
+//  CalendarAITests.swift
+//  CalendarAITests
+//
+//  Created by François Proust--Juvin on 16/08/2025.
+//
+
+import Testing
+
+struct CalendarAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
